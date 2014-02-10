83bcb97161dc909b864b987c308e662134295cad
pradeep <kpgoud533@gmail.com>
2014-02-10 22:23:12 +0530

    Mobile Check-in.

--- trunk/NeEDApp/src/main/java/com/apeironsol/need/core/portal/filter/BranchSelectionCheckFilter.java
+++ trunk/NeEDApp/src/main/java/com/apeironsol/need/core/portal/filter/BranchSelectionCheckFilter.java
@@ -28,6 +28,7 @@ import org.springframework.web.filter.GenericFilterBean;
 
 import com.apeironsol.need.core.portal.SessionBean;
 import com.apeironsol.need.security.portal.BranchesAppStatusBean;
+import com.apeironsol.need.util.DeviceDetectorBean;
 
 @Repository
 public class BranchSelectionCheckFilter extends GenericFilterBean {
@@ -37,40 +38,43 @@ public class BranchSelectionCheckFilter extends GenericFilterBean {
 	@Override
 	public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
 
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
+		final HttpServletRequest request = (HttpServletRequest) req;
+		final HttpServletResponse response = (HttpServletResponse) res;
 
 		// Getting web application context.
-		WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getSession().getServletContext());
+		final WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getSession().getServletContext());
 
 		// Get Filter security intercepter
-		FilterSecurityInterceptor fsi = wac.getBean(FilterSecurityInterceptor.class);
+		final FilterSecurityInterceptor fsi = wac.getBean(FilterSecurityInterceptor.class);
 
 		// Get config attributes.
-		FilterInvocation filterInvocation = new FilterInvocation(request, response, chain);
-		Collection<ConfigAttribute> attributes = fsi.getSecurityMetadataSource().getAttributes(filterInvocation);
+		final FilterInvocation filterInvocation = new FilterInvocation(request, response, chain);
+		final Collection<ConfigAttribute> attributes = fsi.getSecurityMetadataSource().getAttributes(filterInvocation);
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		if (attributes != null) {
-			Collection<GrantedAuthority> grantedAuthorities = authentication.getAuthorities();
-			boolean isSecured = this.isSecured(attributes, grantedAuthorities);
-			if (isSecured && filterInvocation.getRequestUrl().contains("pages/branch/")) {
-
-				SessionBean sessionBean = wac.getBean(SessionBean.class);
-
-				BranchesAppStatusBean branchesAppStatusBean = wac.getBean(BranchesAppStatusBean.class);
-
-				if (sessionBean == null || sessionBean.getCurrentBranch() == null || sessionBean.getCurrentBranch().getId() == null
-						|| sessionBean.getCurrentBranch().getId() == 0
+			final Collection<GrantedAuthority> grantedAuthorities = authentication.getAuthorities();
+			final boolean isSecured = this.isSecured(attributes, grantedAuthorities);
+			if (isSecured && (filterInvocation.getRequestUrl().contains("pages/branch/"))) {
+				final DeviceDetectorBean deviceDetBean = wac.getBean(DeviceDetectorBean.class);
+				final SessionBean sessionBean = wac.getBean(SessionBean.class);
+				final BranchesAppStatusBean branchesAppStatusBean = wac.getBean(BranchesAppStatusBean.class);
+				if ((sessionBean == null) || (sessionBean.getCurrentBranch() == null) || (sessionBean.getCurrentBranch().getId() == null)
+						|| (sessionBean.getCurrentBranch().getId() == 0)
 						|| !branchesAppStatusBean.getBranchesStatus().containsKey(sessionBean.getCurrentBranch().getId())
 						|| !branchesAppStatusBean.getBranchesStatus().get(sessionBean.getCurrentBranch().getId())) {
 
 					logger.info("Branch selection is Requried.");
-					PortResolver portResolver = new PortResolverImpl();
-					DefaultSavedRequest defaultSavedRequest = new DefaultSavedRequest(request, portResolver);
+					final PortResolver portResolver = new PortResolverImpl();
+					final DefaultSavedRequest defaultSavedRequest = new DefaultSavedRequest(request, portResolver);
 					request.getSession().setAttribute(WebAttributes.SAVED_REQUEST, defaultSavedRequest);
-					response.sendRedirect(request.getContextPath() + "/login.html");
+					if (deviceDetBean.getIsMobile()) {
+						response.sendRedirect(request.getContextPath() + "/mobile_login.html");
+					} else {
+						response.sendRedirect(request.getContextPath() + "/login.html");
+					}
+
 				}
 
 			}
@@ -79,10 +83,10 @@ public class BranchSelectionCheckFilter extends GenericFilterBean {
 	}
 
 	private boolean isSecured(final Collection<ConfigAttribute> attributes, final Collection<GrantedAuthority> grantedAuthorities) {
-		for (ConfigAttribute attribute : attributes) {
+		for (final ConfigAttribute attribute : attributes) {
 
 			// Attempt to find a matching granted authority
-			for (GrantedAuthority authority : grantedAuthorities) {
+			for (final GrantedAuthority authority : grantedAuthorities) {
 				if (attribute.getAttribute().equals(authority.getAuthority())) {
 					return true;
 				}
