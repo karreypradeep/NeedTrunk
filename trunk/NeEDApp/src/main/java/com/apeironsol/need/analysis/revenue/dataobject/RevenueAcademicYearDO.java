ed8a8b2c435e6b6c4e40c4b4fadbbe9688d69bc7
pradeep <kpgoud533@gmail.com>
2014-03-09 12:26:27 +0530

    Revert "Merge branch 'master' of https://github.com/karreypradeep/NeedTrunk"
    This reverts commit bdeb8ef3ec1a32b7d9d50701a9a250ff0ad0dc92, reversing
    changes made to 2a3629ebccec717172f8869dd87fb2cc9302ddea.

---
+++ trunk/NeEDApp/src/main/java/com/apeironsol/need/analysis/revenue/dataobject/RevenueAcademicYearDO.java
@@ -0,0 +1,215 @@
+package com.apeironsol.need.analysis.revenue.dataobject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.apeironsol.need.core.model.Branch;
+import com.apeironsol.need.core.model.Klass;
+import com.apeironsol.need.core.model.Relation;
+import com.apeironsol.need.core.model.StudentAcademicYearFeeSummary;
+import com.apeironsol.need.util.constants.GenderConstant;
+
+/**
+ * 
+ * @author pradeep
+ * 
+ */
+public class RevenueAcademicYearDO implements Serializable {
+
+	/**
+	 * Universal serial version id for this class.
+	 */
+	private static final long							serialVersionUID				= -5240957868002122742L;
+
+	/**
+	 * Branch.
+	 */
+	private Branch										branch;
+
+	private Collection<StudentAcademicYearFeeSummary>	studentAcademicYearFeeSummaries	= null;
+
+	private Map<Klass, Collection<Long>>				studentAcademicYearIdsByKlass	= null;
+
+	private Map<Long, Collection<Relation>>				relationsByStudentId			= null;
+
+	/**
+	 * @return the studentAcademicYearFeeSummaries
+	 */
+	public Collection<StudentAcademicYearFeeSummary> getStudentAcademicYearFeeSummaries() {
+		return this.studentAcademicYearFeeSummaries;
+	}
+
+	/**
+	 * @return the studentAcademicYearIdsByKlass
+	 */
+	public Map<Klass, Collection<Long>> getStudentAcademicYearIdsByKlass() {
+		return this.studentAcademicYearIdsByKlass;
+	}
+
+	/**
+	 * @return the relationsBystudentAcademicYearId
+	 */
+	public Map<Long, Collection<Relation>> getRelationsByStudentId() {
+		return this.relationsByStudentId;
+	}
+
+	/**
+	 * @return the branch
+	 */
+	public Branch getBranch() {
+		return this.branch;
+	}
+
+	/**
+	 * @param branch
+	 *            the branch to set
+	 */
+	public void setBranch(final Branch branch) {
+		this.branch = branch;
+	}
+
+	/**
+	 * @param studentAcademicYearFeeSummaries
+	 *            the studentAcademicYearFeeSummaries to set
+	 */
+	public void setStudentAcademicYearFeeSummaries(final Collection<StudentAcademicYearFeeSummary> studentAcademicYearFeeSummaries) {
+		this.studentAcademicYearFeeSummaries = studentAcademicYearFeeSummaries;
+	}
+
+	/**
+	 * @param studentAcademicYearIdsByKlass
+	 *            the studentAcademicYearIdsByKlass to set
+	 */
+	public void setStudentAcademicYearIdsByKlass(final Map<Klass, Collection<Long>> studentAcademicYearIdsByKlass) {
+		this.studentAcademicYearIdsByKlass = studentAcademicYearIdsByKlass;
+	}
+
+	/**
+	 * @param relationsByStudentId
+	 *            the relationsByStudentId to set
+	 */
+	public void setRelationsByStudentId(final Map<Long, Collection<Relation>> relationsByStudentId) {
+		this.relationsByStudentId = relationsByStudentId;
+	}
+
+	public Map<Klass, Double> getRevenueByCourse() {
+		final Map<Klass, Double> revenueByCourse = new HashMap<Klass, Double>();
+		if (this.studentAcademicYearFeeSummaries != null) {
+			final Map<Long, StudentAcademicYearFeeSummary> studentAcademicYearFeeSummaryByStudentAcademicYearId = new HashMap<Long, StudentAcademicYearFeeSummary>();
+			for (final StudentAcademicYearFeeSummary studentAcademicYearFeeSummary : this.studentAcademicYearFeeSummaries) {
+				studentAcademicYearFeeSummaryByStudentAcademicYearId.put(studentAcademicYearFeeSummary.getStudentAcademicYear().getId(),
+						studentAcademicYearFeeSummary);
+			}
+
+			double revenueByKlass = 0;
+			if (this.studentAcademicYearIdsByKlass != null) {
+				for (final Map.Entry<Klass, Collection<Long>> entry : this.studentAcademicYearIdsByKlass.entrySet()) {
+					revenueByKlass = 0;
+					for (final Long studentAcademicYearId : entry.getValue()) {
+						final StudentAcademicYearFeeSummary studentAcademicYearFeeSummary = studentAcademicYearFeeSummaryByStudentAcademicYearId
+								.get(studentAcademicYearId);
+						if (studentAcademicYearFeeSummary != null) {
+							revenueByKlass += studentAcademicYearFeeSummary.getTotalFeePaid() != null ? studentAcademicYearFeeSummary.getTotalFeePaid() : 0;
+						}
+					}
+					revenueByCourse.put(entry.getKey(), revenueByKlass);
+				}
+			}
+		}
+		return revenueByCourse;
+	}
+
+	public Map<GenderConstant, Double> getRevenueByGender() {
+		final Map<GenderConstant, Double> revenueByGender = new HashMap<GenderConstant, Double>();
+		if (this.studentAcademicYearFeeSummaries != null) {
+			for (final StudentAcademicYearFeeSummary studentAcademicYearFeeSummary : this.studentAcademicYearFeeSummaries) {
+				if (revenueByGender.get(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getGender()) != null) {
+					double currentAmount = revenueByGender.get(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getGender());
+					currentAmount += studentAcademicYearFeeSummary.getTotalFeePaid() != null ? studentAcademicYearFeeSummary.getTotalFeePaid() : 0;
+					revenueByGender.put(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getGender(), currentAmount);
+				} else {
+					revenueByGender.put(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getGender(),
+							studentAcademicYearFeeSummary.getTotalFeePaid() != null ? studentAcademicYearFeeSummary.getTotalFeePaid() : 0);
+				}
+			}
+		}
+		return revenueByGender;
+	}
+
+	public Map<String, Double> getRevenueByLocation() {
+		final Map<String, Double> revenueByLocation = new HashMap<String, Double>();
+		if (this.studentAcademicYearFeeSummaries != null) {
+			// As number of zi codes can be many, only display those pincodes
+			// where substantial students are coming from.
+			final Map<String, Integer> locationCount = new HashMap<String, Integer>();
+			for (final StudentAcademicYearFeeSummary studentAcademicYearFeeSummary : this.studentAcademicYearFeeSummaries) {
+				if (locationCount.get(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress().getZipCode()) != null) {
+					int count = locationCount.get(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress().getZipCode());
+					count += 1;
+					locationCount.put(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress().getZipCode(), count);
+				} else {
+					locationCount.put(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress().getZipCode(), 1);
+				}
+			}
+			final Collection<String> locationsUsedInGraph = new ArrayList<String>();
+			if (locationCount.size() <= 10) {
+				for (final Map.Entry<String, Integer> entry : locationCount.entrySet()) {
+					locationsUsedInGraph.add(entry.getKey());
+				}
+			} else {
+				final Map<String, Integer> sortedMap = sortByComparator(locationCount);
+				int count = 1;
+				for (final Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+					if (count > 10) {
+						break;
+					}
+					locationsUsedInGraph.add(entry.getKey());
+					count++;
+				}
+			}
+			for (final StudentAcademicYearFeeSummary studentAcademicYearFeeSummary : this.studentAcademicYearFeeSummaries) {
+				final String zipCode = locationsUsedInGraph.contains(studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress()
+						.getZipCode()) ? studentAcademicYearFeeSummary.getStudentAcademicYear().getStudent().getAddress().getZipCode() : "Other";
+				if (revenueByLocation.get(zipCode) != null) {
+					double currentAmount = revenueByLocation.get(zipCode);
+					currentAmount += studentAcademicYearFeeSummary.getTotalFeePaid() != null ? studentAcademicYearFeeSummary.getTotalFeePaid() : 0;
+					revenueByLocation.put(zipCode, currentAmount);
+				} else {
+					revenueByLocation.put(zipCode, studentAcademicYearFeeSummary.getTotalFeePaid() != null ? studentAcademicYearFeeSummary.getTotalFeePaid()
+							: 0);
+				}
+			}
+		}
+		return revenueByLocation;
+	}
+
+	private static Map<String, Integer> sortByComparator(final Map<String, Integer> unsortMap) {
+
+		final List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+		// sort list based on comparator
+		Collections.sort(list, new Comparator() {
+			@Override
+			public int compare(final Object o1, final Object o2) {
+				return ((Comparable) ((Map.Entry<String, Integer>) (o1)).getValue()).compareTo(((Map.Entry<String, Integer>) (o2)).getValue());
+			}
+		});
+
+		// put sorted list into map again
+		// LinkedHashMap make sure order in which keys were inserted
+		final Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (final Entry<String, Integer> entry : list) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		return sortedMap;
+	}
+}
---
+++ trunk/NeEDApp/src/main/java/com/apeironsol/need/analysis/revenue/portal/RevenueAcademicYearAnalysisBean.java
@@ -0,0 +1,213 @@
+/**
+ * This document is a part of the source code and related artifacts for
+ * SMSystem.
+ * www.apeironsol.com
+ * Copyright © 2013 apeironsol
+ * 
+ */
+package com.apeironsol.need.analysis.revenue.portal;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
+import javax.inject.Named;
+
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.PieChartModel;
+import org.springframework.context.annotation.Scope;
+
+import com.apeironsol.need.analysis.revenue.dataobject.RevenueAcademicYearDO;
+import com.apeironsol.need.analysis.revenue.service.RevenueAnalysisService;
+import com.apeironsol.need.core.model.Klass;
+import com.apeironsol.need.core.portal.AbstractTabbedBean;
+import com.apeironsol.need.util.constants.GenderConstant;
+import com.apeironsol.need.util.constants.RevenueAnalysisTypeConstant;
+import com.apeironsol.need.util.portal.ViewUtil;
+import com.apeironsol.need.util.searchcriteria.RevenueAnalysisSearchCriteria;
+
+/**
+ * JSF managed for financial expense.
+ * 
+ * @author Pradeep
+ */
+@Named
+@Scope(value = "session")
+public class RevenueAcademicYearAnalysisBean extends AbstractTabbedBean implements Serializable {
+
+	/**
+	 * Unique serial version id for this class.
+	 */
+	private static final long				serialVersionUID					= -5693793986889845131L;
+
+	/**
+	 * Branch assert used for searching.
+	 */
+	private RevenueAnalysisSearchCriteria	revenueAnalysisSearchCriteria		= null;
+
+	@Resource
+	private RevenueAnalysisService			revenueAnalysisService;
+
+	private RevenueAcademicYearDO			revenueAcademicYearDO;
+
+	private CartesianChartModel				revenueAcademicYearAnalysisChart	= new CartesianChartModel();
+
+	private PieChartModel					revenueAcademicYearAnalysisPieChart	= new PieChartModel();
+
+	private boolean							displayCharts;
+
+	@PostConstruct
+	public void init() {
+		this.initializeSearchCriteria();
+	}
+
+	public void initializeSearchCriteria() {
+		if (this.revenueAnalysisSearchCriteria == null) {
+			this.revenueAnalysisSearchCriteria = new RevenueAnalysisSearchCriteria(this.sessionBean.getCurrentBranch());
+		}
+	}
+
+	@Override
+	public void onTabChange() {
+
+	}
+
+	/**
+	 * @return the revenueAnalysisSearchCriteria
+	 */
+	public RevenueAnalysisSearchCriteria getRevenueAnalysisSearchCriteria() {
+		return this.revenueAnalysisSearchCriteria;
+	}
+
+	public String resetSearchCriteria() {
+		this.revenueAnalysisSearchCriteria.resetSeacrhCriteria();
+		return null;
+	}
+
+	public String generateReport() {
+
+		if (this.revenueAnalysisSearchCriteria.isSearchCriteriaIsEmpty()) {
+			ViewUtil.addMessage("Please enter search criteria.", FacesMessage.SEVERITY_ERROR);
+		} else {
+			this.revenueAnalysisSearchCriteria.setBranch(this.sessionBean.getCurrentBranch());
+			this.revenueAcademicYearDO = this.revenueAnalysisService.getRevenueGeneratedForAcademicYearBySearchCriteria(this.revenueAnalysisSearchCriteria);
+			this.generateRevenueReport();
+		}
+		return null;
+	}
+
+	/**
+	 * @return the revenueAcademicYearDO
+	 */
+	public RevenueAcademicYearDO getRevenueAcademicYearDO() {
+		return this.revenueAcademicYearDO;
+	}
+
+	/**
+	 * @param revenueAcademicYearDO
+	 *            the revenueAcademicYearDO to set
+	 */
+	public void setRevenueAcademicYearDO(final RevenueAcademicYearDO revenueAcademicYearDO) {
+		this.revenueAcademicYearDO = revenueAcademicYearDO;
+	}
+
+	/**
+	 * Subject exam results.
+	 */
+	public void generateRevenueReport() {
+		this.revenueAcademicYearAnalysisChart = new CartesianChartModel();
+		this.revenueAcademicYearAnalysisPieChart = new PieChartModel();
+		if (this.revenueAcademicYearDO != null) {
+			if (RevenueAnalysisTypeConstant.BY_COURSE.equals(this.revenueAnalysisSearchCriteria.getRevenueAnalysisType())) {
+				this.generateChartByCourse();
+			} else if (RevenueAnalysisTypeConstant.BY_GENDER.equals(this.revenueAnalysisSearchCriteria.getRevenueAnalysisType())) {
+				this.generateChartByGender();
+			} else if (RevenueAnalysisTypeConstant.BY_LOCATION.equals(this.revenueAnalysisSearchCriteria.getRevenueAnalysisType())) {
+				this.generateChartByLocation();
+			}
+			this.displayCharts = true;
+		}
+	}
+
+	private void generateChartByCourse() {
+		final Map<Klass, Double> expensesMapByType = this.revenueAcademicYearDO.getRevenueByCourse();
+		final ChartSeries percentage = new ChartSeries();
+		percentage.setLabel("Revenue");
+		for (final Map.Entry<Klass, Double> entry : expensesMapByType.entrySet()) {
+			percentage.set(entry.getKey().getName(), entry.getValue());
+			this.revenueAcademicYearAnalysisPieChart.set(entry.getKey().getName(), entry.getValue());
+		}
+		this.revenueAcademicYearAnalysisChart.addSeries(percentage);
+	}
+
+	private void generateChartByGender() {
+		final Map<GenderConstant, Double> expensesMapByType = this.revenueAcademicYearDO.getRevenueByGender();
+		final ChartSeries percentage = new ChartSeries();
+		percentage.setLabel("Revenue");
+		for (final Map.Entry<GenderConstant, Double> entry : expensesMapByType.entrySet()) {
+			percentage.set(entry.getKey().getLabel(), entry.getValue());
+			this.revenueAcademicYearAnalysisPieChart.set(entry.getKey().getLabel(), entry.getValue());
+		}
+		this.revenueAcademicYearAnalysisChart.addSeries(percentage);
+	}
+
+	private void generateChartByLocation() {
+		final Map<String, Double> expensesMapByType = this.revenueAcademicYearDO.getRevenueByLocation();
+		final ChartSeries percentage = new ChartSeries();
+		percentage.setLabel("Revenue");
+		for (final Map.Entry<String, Double> entry : expensesMapByType.entrySet()) {
+			percentage.set(entry.getKey(), entry.getValue());
+			this.revenueAcademicYearAnalysisPieChart.set(entry.getKey(), entry.getValue());
+		}
+		this.revenueAcademicYearAnalysisChart.addSeries(percentage);
+	}
+
+	/**
+	 * @return the revenueAcademicYearAnalysisChart
+	 */
+	public CartesianChartModel getRevenueAcademicYearAnalysisChart() {
+		return this.revenueAcademicYearAnalysisChart;
+	}
+
+	/**
+	 * @param revenueAcademicYearAnalysisChart
+	 *            the revenueAcademicYearAnalysisChart to set
+	 */
+	public void setRevenueAcademicYearAnalysisChart(final CartesianChartModel revenueAcademicYearAnalysisChart) {
+		this.revenueAcademicYearAnalysisChart = revenueAcademicYearAnalysisChart;
+	}
+
+	/**
+	 * @return the revenueAcademicYearAnalysisPieChart
+	 */
+	public PieChartModel getRevenueAcademicYearAnalysisPieChart() {
+		return this.revenueAcademicYearAnalysisPieChart;
+	}
+
+	/**
+	 * @param revenueAcademicYearAnalysisPieChart
+	 *            the revenueAcademicYearAnalysisPieChart to set
+	 */
+	public void setRevenueAcademicYearAnalysisPieChart(final PieChartModel revenueAcademicYearAnalysisPieChart) {
+		this.revenueAcademicYearAnalysisPieChart = revenueAcademicYearAnalysisPieChart;
+	}
+
+	/**
+	 * @return the displayCharts
+	 */
+	public boolean isDisplayCharts() {
+		return this.displayCharts;
+	}
+
+	/**
+	 * @param displayCharts
+	 *            the displayCharts to set
+	 */
+	public void setDisplayCharts(final boolean displayCharts) {
+		this.displayCharts = displayCharts;
+	}
+
+}
